fce0eb3e88232fc54c3cfaa4fbecccbf250e5865
alimakh <112596513+alimakh@users.noreply.github.com>
2023-04-19 02:31:18 -0500

    remove a feature

--- js/script.js
+++ js/script.js
@@ -20,19 +20,6 @@ function initMap() {
         title: 'Chicago, IL'
     });
     
-    var searchInput = document.getElementById('search-input');
-    var autocomplete = new google.maps.places.Autocomplete(searchInput);
-
-    autocomplete.addListener('place_changed', function() {
-        var place = autocomplete.getPlace();
-        if (place.geometry) {
-          map.panTo(place.geometry.location);
-          map.setZoom(15);
-        } else {
-          alert("The place has no geometry!");
-        }
-      });      
-
     var infoWindow = new google.maps.InfoWindow({
         content: 'Welcome to Chicago!'
     });
